92480f70cdb3247201c7818c614d907465a33424
Dominik-Bednorz <bednorzdominik27@gmail.com>
2026-05-29 12:33:09 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 const konvertbuttonstatus = document.querySelector(".kb");
 konvertbuttonstatus.disabled = true;
-
+const hackingMode = false;
 const input = document.getElementById("fotoinput");
+
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "h") {
+  const hackingMode = true;
+  localStorage.setItem("hackingMode", hackingMode);
+  location.reload(); //beta
+  }
+  if (event.key === "n") {
+  const hackingMode = false;
+  localStorage.setItem("hackingMode", false);
+  location.reload(); //beta
+  }
+});
+// location.reload(); um die Seite neu zu laden, damit die Änderungen sofort sichtbar sind
+console.log("Hacking Mode: jetz var " + hackingMode);
+console.log("Hacking Mode: speicher" + localStorage.getItem("hackingMode"));
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (hackingMode || localStorage.getItem("hackingMode") === "true") {
+    document.body.style.background = "black";
+    document.body.style.color = "lime";
+    document.body.style.fontFamily = "Courier New, monospace";
+    document.body.style.textShadow = "0 0 5px lime, 0 0 10px lime, 0 0 20px lime";
+  }
+});
+
+
 
 input.addEventListener("change", () => {
   const name = input.files[0].name;
